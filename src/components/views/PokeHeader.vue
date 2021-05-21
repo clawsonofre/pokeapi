7c059bb54b6bf135c8e9262b00b74e398b0cb496
Alejandro Noriega <clawsonofre@gmail.com>
2021-05-21 00:23:58 -0500

    added folder views

---
+++ src/components/views/PokeHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="PokeHeader">
+    <div>
+      <b-navbar class="p-1" toggleable="sm" type="dark" variant="dark">
+        <router-link to="pokedex">
+          <b-navbar-brand>Pokedex</b-navbar-brand>
+        </router-link>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      </b-navbar>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#PokeHeader {
+  z-index: 1005;
+}
+</style>
