3bf1c80554329f9545d64d781458f1df08d4189b
Alejandro Noriega <clawsonofre@gmail.com>
2021-05-24 23:40:49 -0500

    Renamed the class ID in PokeFooter.vue}

--- src/components/views/PokeFooter.vue
+++ src/components/views/PokeFooter.vue
@@ -1,19 +1,18 @@
 <template>
   <!-- Footer -->
-  <footer id="footer">
+  <footer id="PokeFooter">
     <div class="container">
-      <div class="credits">
         Designed by
         <a target="blank" href="https://www.linkedin.com/in/alejandro-noriega">Alejandro Noriega</a><br>
         <a target="blank" href="https://vuejs.org/">VueJS</a> <a target="blank" href="https://pokeapi.co/">PokeApi</a> <a target="blank" href="https://github.com/imcvampire/vue-axios#readme">Axios</a>
         
-      </div>
+      
     </div>
   </footer>
   <!-- Footer -->
 </template>
 <style scoped>
-footer {
+#PokeFooter {
 background-color: rgb(0, 0, 0);
 }
 </style>
