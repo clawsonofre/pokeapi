74c981cf84c7d41ace6da426768bf38c98f06feb
Alejandro Noriega <clawsonofre@gmail.com>
2021-05-26 22:46:08 -0500

    removed unnecesary tabs

--- src/components/PokeError.vue
+++ src/components/PokeError.vue
@@ -1,12 +1,13 @@
-<template><div class="SearchError">
-          <p >
-            Pokemon Search Error.
-            <br /><br />
-            <img src="../assets/img/error.png" />
-          </p></div>
+<template>
+  <div class="SearchError">
+    <p>
+      Pokemon Search Error. <br /><br />
+      <img src="../assets/img/error.png" />
+    </p>
+  </div>
 </template>
 <style scoped>
-.SearchError img{
-    width: "width: 10vw";
+.SearchError img {
+  width: "width: 10vw";
 }
 </style>
